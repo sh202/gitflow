39eaac6f783786b4a27444ef501ca10106512298
bear-kings <2663341052@qq.com>
2020-01-01 17:12:38 +0800

    添加了like模型

--- like/models.py
+++ like/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class like(models.Model):
+    uid = models.IntegerField()
+    wid = models.IntegerField()
+    created = models.DateTimeFileld()
